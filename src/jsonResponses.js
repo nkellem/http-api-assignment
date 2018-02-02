4d26074ca0110334b29c1181271c1b8441d168af
nkellem <noahkellem@gmail.com>
2018-02-02 10:07:21 -0500

    Basic routing implemented

---
+++ src/jsonResponses.js
@@ -0,0 +1,90 @@
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const success = (request, response) => {
+  const responseJSON = {
+    message: 'This is a successful response',
+    id: 'success',
+  };
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const badRequest = (request, response, params) => {
+  const responseJSON = {
+    message: 'This request has the required parameters',
+  };
+
+  if (!params.valid || params.valid !== 'true') {
+    responseJSON.message = 'missing valid query paramter set to true';
+    responseJSON.id = 'badRequest';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  return respondJSON(request, response, 200, responseJSON);
+
+};
+
+const unauthorized = (request, response, params) => {
+  const responseJSON = {
+    message: 'You are authorized',
+  };
+
+  if (!params.loggedIn || params.loggedIn !== 'yes') {
+    responseJSON.message = 'You are not logged in; unauthorized';
+    responseJSON.id = 'unauthorized';
+    return respondJSON(request, response, 401, responseJSON);
+  }
+
+  return respondJSON(request, response, 200, responseJSON);
+
+};
+
+const forbidden = (request, response) => {
+  const responseJSON = {
+    message: 'You are forbidden from viewing this content',
+    id: 'forbidden',
+  };
+
+  return respondJSON(request, response, 403, responseJSON);
+};
+
+const internal = (request, response) => {
+  const responseJSON = {
+    message: 'Internal server error',
+    id: 'internal',
+  };
+
+  return respondJSON(request, response, 500, responseJSON);
+};
+
+const notImplemented = (request, response) => {
+  const responseJSON = {
+    message: 'Request method not supported by the server',
+    id: 'notImplemented',
+  };
+
+  respondJSON(request, response, 501, responseJSON);
+}
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+module.exports = {
+  success,
+  badRequest,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
+  notFound,
+};
